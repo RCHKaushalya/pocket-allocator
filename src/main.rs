a689cc0a512ac5ddee8835e3c1208fb5344bea7d
RCHKaushalya <rchkaushalya@gmail.com>
2025-06-22 11:46:28 +0530

    Step 1
    chore: laid the foundation for our custom heap 🧱 added static memory buffer and block structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+const HEAP_SIZE: usize = 1024;
+static mut HEAP: [u8; HEAP_SIZE] = [0; HEAP_SIZE];
+
+#[derive(Clone, Copy, Debug)]
+struct Block {
+    start: usize,
+    size: usize,
+    is_free: bool,
+}
+
+static mut BLOCKS: [Block; 32] = [Block {start: 0, size: 0, is_free: true}; 32];
+
 fn main() {
     println!("Hello, world!");
 }
